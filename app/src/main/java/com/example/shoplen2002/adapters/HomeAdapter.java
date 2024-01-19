8e9a6078db72968421ab59bee68de457d6cf1011
hoangthuy0902 <104625264+hoangthuy0902@users.noreply.github.com>
2024-01-19 18:17:17 +0700

    homecategories

---
+++ app/src/main/java/com/example/shoplen2002/adapters/HomeAdapter.java
@@ -0,0 +1,55 @@
+package com.example.shoplen2002.adapters;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.bumptech.glide.Glide;
+import com.example.shoplen2002.R;
+import com.example.shoplen2002.models.HomeCategories;
+
+import java.util.List;
+
+public class HomeAdapter extends RecyclerView.Adapter<HomeAdapter.ViewHolder> {
+    Context context;
+    List<HomeCategories> categoriesList;
+
+    public HomeAdapter(Context context, List<HomeCategories> categoriesList) {
+        this.context = context;
+        this.categoriesList = categoriesList;
+    }
+
+    @NonNull
+    @Override
+    public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        return new ViewHolder(LayoutInflater.from(parent.getContext()).inflate(R.layout.home_cat_item,parent,false));
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull HomeAdapter.ViewHolder holder, int position) {
+        Glide.with(context).load(categoriesList.get(position).getImg_url()).into(holder.catImg);
+        holder.name.setText(categoriesList.get(position).getName());
+    }
+
+    @Override
+    public int getItemCount() {
+        return categoriesList.size();
+    }
+    public class ViewHolder extends RecyclerView.ViewHolder {
+        ImageView catImg;
+        TextView name;
+
+
+        public ViewHolder(@NonNull View itemView) {
+            super(itemView);
+            catImg = itemView.findViewById(R.id.home_cat_img);
+            name = itemView.findViewById(R.id.home_cat_name);
+        }
+    }
+}
--- app/src/main/java/com/example/shoplen2002/ui/home/HomeFragment.java
+++ app/src/main/java/com/example/shoplen2002/ui/home/HomeFragment.java
@@ -14,9 +14,13 @@ import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.example.shoplen2002.R;
+import com.example.shoplen2002.adapters.HomeAdapter;
 import com.example.shoplen2002.adapters.PopularAdapters;
+import com.example.shoplen2002.adapters.RecommendedAdapter;
 import com.example.shoplen2002.databinding.FragmentHomeBinding;
+import com.example.shoplen2002.models.HomeCategories;
 import com.example.shoplen2002.models.PopularModel;
+import com.example.shoplen2002.models.RecommendedModel;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.FirebaseAuth;
@@ -29,12 +33,18 @@ import java.util.List;
 
 public class HomeFragment extends Fragment {
 
-    RecyclerView popularRec;
+    RecyclerView popularRec, recommendedRec, homeCatRec;
     FirebaseFirestore db;
 
     //popular item
-   List<PopularModel> popularModelList;
-   PopularAdapters popularAdapters;
+    List<PopularModel> popularModelList;
+    PopularAdapters popularAdapters;
+    //home catrgories
+    List<HomeCategories> categoriesList;
+    HomeAdapter homeAdapter;
+    //recommended item
+    List<RecommendedModel> recommendedModelList;
+    RecommendedAdapter recommendedAdapter;
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
 
@@ -42,6 +52,8 @@ public class HomeFragment extends Fragment {
         db = FirebaseFirestore.getInstance();
 
         popularRec = root.findViewById(R.id.pop_rec);
+        homeCatRec = root.findViewById(R.id.explore_rec);
+        recommendedRec = root.findViewById(R.id.recommended_rec);
 
         //Popular items
         popularRec.setLayoutManager(new LinearLayoutManager(getActivity(),RecyclerView.HORIZONTAL,false));
@@ -60,6 +72,52 @@ public class HomeFragment extends Fragment {
                                 PopularModel popularModel = document.toObject(PopularModel.class);
                                 popularModelList.add(popularModel);
                                 popularAdapters.notifyDataSetChanged();
+                            }
+                        }else {
+                            Toast.makeText(getActivity(), "Error"+task.getException(), Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                });
+        //Home categories
+        homeCatRec.setLayoutManager(new LinearLayoutManager(getActivity(),RecyclerView.HORIZONTAL,false));
+        categoriesList = new ArrayList<>();
+        homeAdapter = new HomeAdapter(getActivity(),categoriesList);
+        homeCatRec.setAdapter(homeAdapter);
+
+        db.collection("HomeCategories")
+                .get()
+                .addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+                    @Override
+                    public void onComplete(@NonNull Task<QuerySnapshot> task) {
+                        if(task.isSuccessful()){
+                            for(QueryDocumentSnapshot document: task.getResult()){
+
+                                HomeCategories homeCategories = document.toObject(HomeCategories.class);
+                                categoriesList.add(homeCategories);
+                                homeAdapter.notifyDataSetChanged();
+                            }
+                        }else {
+                            Toast.makeText(getActivity(), "Error"+task.getException(), Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                });
+        //Recommended items
+        recommendedRec.setLayoutManager(new LinearLayoutManager(getActivity(),RecyclerView.HORIZONTAL,false));
+        recommendedModelList = new ArrayList<>();
+        recommendedAdapter = new RecommendedAdapter(getActivity(),recommendedModelList);
+        recommendedRec.setAdapter(recommendedAdapter);
+
+        db.collection("Recommended")
+                .get()
+                .addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+                    @Override
+                    public void onComplete(@NonNull Task<QuerySnapshot> task) {
+                        if(task.isSuccessful()){
+                            for(QueryDocumentSnapshot document: task.getResult()){
+
+                                RecommendedModel recommendedModel = document.toObject(RecommendedModel.class);
+                                recommendedModelList.add(recommendedModel);
+                                recommendedAdapter.notifyDataSetChanged();
                             }
                         }else {
                             Toast.makeText(getActivity(), "Error"+task.getException(), Toast.LENGTH_SHORT).show();
